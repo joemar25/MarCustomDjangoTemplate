b0c3f3641e0154bfd7021f549c17dfd2b741d3df
joemar25 <joemar.cardino000@gmail.com>
2023-06-22 21:06:00 +0800

    init

--- application/_root/urls.py
+++ application/_root/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blogs/', include('application.blogs.urls')),
+    # path('admin/', admin.site.urls),
+    path('', include('application.blogs.urls')),
 ]
