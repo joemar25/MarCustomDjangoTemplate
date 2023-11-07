f5ff6eb428bae74b76b08879011558fb48210282
joemar25 <joemar.cardino000@gmail.com>
2023-11-07 19:25:45 +0800

    Fix formatting in manage.py and remove unused SVG file

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application._core.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "application._core.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,5 +15,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
