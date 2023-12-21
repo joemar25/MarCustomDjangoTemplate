29e1771031a35284dfced6b5f5572139bd1b3891
joemar25 <joemar.cardino000@gmail.com>
2023-12-21 16:05:11 +0800

    Update CSS styles and HTML structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,7 @@ export default {
   content: ["./application/**/*.{html,js}"],
   darkMode: "class",
   theme: {
-    extend: {
-      // Extended color customizations, example text-brown-50, bg-customPrimary, etc.
-      colors: {
-        brown: {
-          50: "#fdf8f6",
-          100: "#f2e8e5",
-          200: "#eaddd7",
-          300: "#e0cec7",
-          400: "#d2bab0",
-          500: "#bfa094",
-          600: "#a18072",
-          700: "#977669",
-          800: "#846358",
-          900: "#43302b",
-        },
-        customPrimary: "#00ADB5",
-        customSecondary: "#393E46",
-        customLight: "#EEEEEE",
-        customDark: "#222831",
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
